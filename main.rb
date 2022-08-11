c22fbfbc6c7f6b64495a207a6266e4f19dd7cdc2
Nihal Sahu <iamnihalsahu@gmail.com>
2022-08-11 19:58:04 +0530

    this works?

--- main.rb
+++ main.rb
@@ -9,43 +9,43 @@ x = 0
 
 # Iterate through each file
 Dir.foreach('./files') do |filename|
-    # Dir.foreach returns the directory name and the pathname to the previous directory
-    next if filename == '.' or filename == '..'
-    x+= 1
+  # Dir.foreach returns the directory name and the pathname to the previous directory
+  next if filename == '.' or filename == '..'
+  x+= 1
 
-    reader = PDF::Reader.new("./files/#{filename}")
-    
-    text = reader.pages.map {|page| page.text}
-    text = text.join
-    citation = text.match('^([^\s]+)')
-    appellants = find_text(text, 'Appellants:', 'Vs.')
-    respondents = find_text(text, 'Respondent:', "Hon\'ble Judges/Coram:\n")
-    # Go to the judgment, 
-    puts "----------------------------------------------------"
-    if text.include?("Case Note:\n")
-        puts "THE CASE NOTE HAS BEEN FOUND"
-        if text.include?("ORDER\n")
-            puts "THIS FILE IS AN ORDER"
-            casenote = find_text(text, "Case Note:\n", "ORDER\n")
-        elsif text.include?("JUDGMENT\n")
-            puts "THIS FILE IS A JUDGMENT"
-            casenote = find_text(text, "Case Note:\n", "JUDGMENT\n")
-        end
-    else
-        casenote = 'COULD NOT FIND CASE NOTE'
-    end
-    puts "------------------------------------------------------"
+  reader = PDF::Reader.new("./files/#{filename}")
+  
+  text = reader.pages.map {|page| page.text}
+  text = text.join
+  citation = text.match('^([^\s]+)')
+  appellants = find_text(text, 'Appellants:', 'Vs.')
+  respondents = find_text(text, 'Respondent:', "Hon\'ble Judges/Coram:\n")
+  # Go to the judgment, 
+  puts "----------------------------------------------------"
+  if text.include?("Case Note:\n")
+      puts "THE CASE NOTE HAS BEEN FOUND"
+      if text.include?("ORDER\n")
+          puts "THIS FILE IS AN ORDER"
+          casenote = find_text(text, "Case Note:\n", "ORDER\n")
+      elsif text.include?("JUDGMENT\n")
+          puts "THIS FILE IS A JUDGMENT"
+          casenote = find_text(text, "Case Note:\n", "JUDGMENT\n")
+      end
+  else
+      casenote = 'COULD NOT FIND CASE NOTE'
+  end
+  puts "------------------------------------------------------"
 
-    puts "#{x}: #{appellants} v. #{respondents} (#{citation})"
-    workbook = RubyXL::Parser.parse('./test.xlsx')
+  puts "#{x}: #{appellants} v. #{respondents} (#{citation})"
+  workbook = RubyXL::Parser.parse('./test.xlsx')
 
-    worksheet = workbook[0]
-    worksheet[x][1] = citation
-    worksheet[x][2] = appellants
-    worksheet[x][3] = respondents
-    worksheet[x][4] = casenote
+  worksheet = workbook[0]
+  
+  worksheet.sheet_data[x][0].change_contents(citation)
+  worksheet.sheet_data[x][1].change_contents(appellants)
+  worksheet.sheet_data[x][2].change_contents(respondents)
+  worksheet.sheet_data[x][3].change_contents(casenote)
 
-    worksheet.reload
-    
-    workbook.write("./test.xlsx")end 
+  workbook.write("./test.xlsx")
 
+end
